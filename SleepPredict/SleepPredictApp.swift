3058627bba9c27bacd37ee64b92c64152b7e9afc
Luka <vujnovacluka89@gmail.com>
2021-10-15 22:06:11 +0200

    Initial Commit

---
+++ SleepPredict/SleepPredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepPredictApp.swift
+//  SleepPredict
+//
+//  Created by Luka Vujnovac on 15.10.2021..
+//
+
+import SwiftUI
+
+@main
+struct SleepPredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
